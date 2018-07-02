ef84b15801f8a23544bba65fa4a92d972d7afb01
Gottfried Ganßauge <Gottfried.Ganssauge@haufe-lexware.com>
2018-07-02 10:24:42 +0200

    Initial version

---
+++ useful.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#
+# Modify the subject file such that it doesn't longer
+# contain an 'x' for a 'u' 
+#
+# Parameters:
+#    name - name of subject file to fix 
+#
+fix_subject() {
+    local name="$1"
+
+    sed -i 's,x,u'g "$name"
+}
+
+#
+# Create a file containing some interesting subject
+#
+# Parameters:
+#    name - name of subject file to create 
+#
+create_subject() {
+    local name="$1"
+
+    echo 'This is a sxbject' >"$name"
+}
